fdd10eaef4ba8ded8d466671b8800d5fd4f04fc4
msr8 <live.shashwat@gmail.com>
2022-10-04 14:01:23 +0530

    added wallpaper.ps1

---
+++ Shell Scripts/powershell/wallpaper.ps1
@@ -0,0 +1,18 @@
+Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+public class wpchange {
+    public const uint SPI_SETDESKWALLPAPER = 0x0014;
+    public const uint SPIF_UPDATEINIFILE = 0x01;
+    public const uint SPIF_SENDWININICHANGE = 0x02;
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
+    public static void SetWallpaper (string path) {
+        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+    }
+}
+'@
+
+New-Object System.Net.WebClient.DownloadFile("https://raw.githubusercontent.com/msr8/attiny85/main/Assets/cat6.jpg", "$env:TEMPDIR\test.jpg")
+
+[wpchange]::SetWallpaper("$env:TEMPDIR\test.jpg")
+
