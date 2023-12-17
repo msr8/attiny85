54ce40a8a6d17a8e076f404691ba9474cd0e0d99
msr8 <live.shashwat@gmail.com>
2023-12-17 17:38:35 +0530

    yamete-1.ps1

--- Shell Scripts/powershell/yamete-1.ps1
+++ Shell Scripts/powershell/yamete-1.ps1
@@ -1,11 +1,13 @@
-$URL = "https://raw.githubusercontent.com/msr8/attiny85/main/Assets/yamete-kudusai.wav"
+$URL = "https://raw.githubusercontent.com/msr8/attiny85/main/Assets/yamete-kudasai.wav"
 $FP  = "$env:TEMP\15791817.wav"
 
-# Download the audio file and save it with a random name in the temporary directory
-# https://stackoverflow.com/a/51225744/17002774
-Invoke-WebRequest -Uri $URL -OutFile $FP
+# Check if the file does not exist
+if ( -not (Test-Path $FP) ) {
+    # if it doesnt exist, download the audio file and save it in the temporary directory
+    # https://stackoverflow.com/a/51225744/17002774
+    Invoke-WebRequest -Uri $URL -OutFile $FP
+}
 
-# https://www.youtube.com/watch?v=kJvl7GjjkI0
 # Play the audio file
 # https://stackoverflow.com/a/73538809/17002774
 (New-Object System.Media.SoundPlayer($FP)).PlaySync()
